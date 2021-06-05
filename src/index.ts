61bc045b73dd3d774c099f5b668eed7ec727ddee
Kegen Guyll <guyllkegen@gmail.com>
2021-06-05 13:28:34 -0500

    feat(app) created functions for all routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,82 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosStatic } from 'axios';
+import { Joke, JokeResponse, JokeResponseArray } from './models/joke';
+
+class DadJokes {
+  config: AxiosRequestConfig;
+  axios: AxiosStatic;
+  constructor(private rapidapi_key: string) {
+    this.config = {
+      headers: {
+        'x-rapidapi-key': this.rapidapi_key,
+        'x-rapidapi-host': 'dad-jokes.p.rapidapi.com',
+      },
+    };
+    this.axios = axios;
+  }
+
+  get authToken(): string {
+    return this.rapidapi_key;
+  }
+
+  set authToken(newAuthToken: string) {
+    this.rapidapi_key = newAuthToken;
+  }
+
+  async randomJokes(count?: number): Promise<Joke[]> {
+    if (count && count > 5) {
+      console.warn('Count limit for random jokes is 5');
+    }
+
+    try {
+      const response: AxiosResponse<JokeResponseArray> = await this.axios.get(
+        `https://dad-jokes.p.rapidapi.com/random/joke?count=${count || 1}`,
+        this.config,
+      );
+
+      return response.data.body;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async jokeById(_id: string): Promise<Joke> {
+    try {
+      const response: AxiosResponse<JokeResponse> = await this.axios.get(
+        `https://dad-jokes.p.rapidapi.com/joke/${_id}`,
+        this.config,
+      );
+
+      return response.data.body;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async jokeByType(type: string, limit?: number): Promise<Joke[]> {
+    try {
+      const response: AxiosResponse<JokeResponseArray> = await this.axios.get(
+        `https://dad-jokes.p.rapidapi.com/joke/type/${type}?limit=${limit || 50}`,
+        this.config,
+      );
+
+      return response.data.body;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async searchJoke(term: string): Promise<Joke[]> {
+    try {
+      const response: AxiosResponse<JokeResponseArray> = await this.axios.get(
+        `https://dad-jokes.p.rapidapi.com/joke/search?term=${term}`,
+        this.config,
+      );
+
+      return response.data.body;
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+export default DadJokes;
