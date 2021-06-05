667a3dba69dbb452ff9f40e77ca0326e61e14ec7
Kegen Guyll <guyllkegen@gmail.com>
2021-06-05 13:28:51 -0500

    feat(mocks) add mock data

---
+++ src/__mocks__/jokeById.mock.ts
@@ -0,0 +1,9 @@
+export const MockJoke = {
+  success: true,
+  body: {
+    _id: '5f80ccd641785ba7c7d27cb3',
+    type: 'general',
+    setup: 'Why does a chicken coop only have two doors?',
+    punchline: 'Because if it had four doors it would be a chicken sedan.',
+  },
+};
